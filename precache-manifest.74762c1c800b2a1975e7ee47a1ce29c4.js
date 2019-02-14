881573f6c4aff7c6a67cbfc4a763cdcc72500385
LE NGOC CUONG <lncnb91@gmail.com>
2019-02-15 01:18:32 +0700

    Updates

---
+++ precache-manifest.74762c1c800b2a1975e7ee47a1ce29c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4bd7f0e17f07438190c",
+    "url": "/moneystat/static/css/main.e8e7157a.chunk.css"
+  },
+  {
+    "revision": "b4bd7f0e17f07438190c",
+    "url": "/moneystat/static/js/main.b4bd7f0e.chunk.js"
+  },
+  {
+    "revision": "a4480bdca58ee80deccd",
+    "url": "/moneystat/static/js/runtime~main.a4480bdc.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/moneystat/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/moneystat/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0689ad803e8d2e4c17462cf9dacaa7b3",
+    "url": "/moneystat/index.html"
+  }
+];
