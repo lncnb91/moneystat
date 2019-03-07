3db1ca148a6fbf42a56a69b644dda36af151909d
le.ngoc.cuong <le.ngoc.cuong@Le-Ngoc-Cuong-Macbook-2018.local>
2019-03-07 16:36:40 +0700

    Convert SpendingByUserChart to React code

--- src/components/Content/ContentBody/MainRow/SpendingByUserChart.js
+++ src/components/Content/ContentBody/MainRow/SpendingByUserChart.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import { Pie } from 'react-chartjs'
 
 class SpendingByUserChart extends Component {
-  render() {
-    return (
+  convertResponseData = (response) => {
+    return response.map(user => ({label: user.user_name, value: user.spent_amount}));
+  }
+
+  
+  render(){
+    return(
       <div className="box box-danger">
         <div className="box-header with-border">
           <h3 className="box-title">Spending by user</h3>
@@ -13,12 +19,15 @@ class SpendingByUserChart extends Component {
             <button type="button" className="btn btn-box-tool" data-widget="remove"><i className="fa fa-times"></i></button>
           </div>
         </div>
-        <div className="box-body">
-          <canvas id="pieChart" style={{height: "250px"}}></canvas>
+        <div className="box-body" style={{textAlign: "center"}}>
+          <Pie
+            data={this.convertResponseData(this.props.spendingByUser)}
+          />
         </div>
       </div>
     );
   }
 }
+
 
 export default SpendingByUserChart
